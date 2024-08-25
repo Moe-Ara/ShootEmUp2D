194c30c992d7d2a0789717f859d6267059efe53a
Mohamad Arabi <mohamadarabi@web.de>
2024-08-26 01:19:23 +0200

    Update crosshair.gd
    removed cursor

--- Scripts/crosshair.gd
+++ Scripts/crosshair.gd
@@ -3,7 +3,7 @@ extends Sprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
